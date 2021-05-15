0103fee24a2a178c6982b66854ea86910670f72e
yasenbr <74763207+yasenbr@users.noreply.github.com>
2021-05-15 18:52:46 +0300

    build message Sender

--- src/component/Feed.js
+++ src/component/Feed.js
@@ -1,11 +1,13 @@
 import React from "react";
 import StoryReel from "./StoryReel";
+import MessageSender from "./MessageSender";
 import "../css/Feed.css";
 
 function Feed() {
   return (
     <div className="feed">
       <StoryReel />
+      <MessageSender />
       {/**StoryReel */}
       {/**MessageSender */}
       {/**MessageSender */}
