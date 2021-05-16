39911c0a0ac9cad2a2f30e58717ba789b06bec7d
yasenbr <74763207+yasenbr@users.noreply.github.com>
2021-05-17 00:45:36 +0300

    add the feed fonctionality (design part)

--- src/component/Feed.js
+++ src/component/Feed.js
@@ -9,6 +9,19 @@ function Feed() {
     <div className="feed">
       <StoryReel />
       <MessageSender />
+      <Post
+        profilePic="https://lh3.googleusercontent.com/ogw/ADGmqu_hh6GWh8mb6MnyqTCLclBLZ8O1ABxmQBUNmeoAXOw=s32-c-mo"
+        message="Dummy info for styling"
+        timestamp="This is a time stamp"
+        username="Yasen Ben Rhayem"
+        image="https://images.unsplash.com/photo-1621173865296-baaf7dc3afcc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+      />
+      <Post
+        profilePic="https://lh3.googleusercontent.com/ogw/ADGmqu_hh6GWh8mb6MnyqTCLclBLZ8O1ABxmQBUNmeoAXOw=s32-c-mo"
+        message="Dummy info for styling"
+        timestamp="This is a time stamp"
+        username="Yasen Ben Rhayem"
+      />
       <Post />
     </div>
   );
--- src/component/Post.js
+++ src/component/Post.js
@@ -1,8 +1,49 @@
+import { Avatar } from "@material-ui/core";
 import React from "react";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ChatBubbleOutlineOutlinedIcon from "@material-ui/icons/ChatBubbleOutlineOutlined";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import "../css/Post.css";
 
-function Post() {
-  return <div className="post"></div>;
+function Post({ profilePic, image, username, timestamp, message }) {
+  return (
+    <div className="post">
+      <div className="post__top">
+        <Avatar src={profilePic} className="post__avatar" />
+        <div className="post__topInfo">
+          <h3>{username}</h3>
+          <p>Timestamp...</p>
+        </div>
+      </div>
+      <div className="post__bottom">
+        <p>{message}</p>
+      </div>
+      <div className="post__image">
+        <img src={image} alt="" />
+      </div>
+
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUpIcon />
+          <p>Like</p>
+        </div>
+        <div className="post__option">
+          <ChatBubbleOutlineOutlinedIcon />
+          <p>Like</p>
+        </div>
+        <div className="post__option">
+          <NearMeIcon />
+          <p>Like</p>
+        </div>
+        <div className="post__option">
+          <AccountCircleIcon />
+          <ExpandMoreIcon />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Post;
